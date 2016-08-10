c06305c77df425169f39f17af58cb97a6eddcb2d
Leona <srt459vn31@gmail.com>
2016-08-10 03:09:10 +0000

    0810

--- PayMent/controllers/DepositLeona.php
+++ PayMent/controllers/DepositLeona.php
@@ -41,7 +41,8 @@
 //錯誤訊息
         public function error ($error) {
             $a = '<div class="alert alert-danger alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                     <h4><strong>'.$error.'</strong></h4></div>';
 
             $this->view("point", Array($a));

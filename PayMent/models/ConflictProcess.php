9dd677725d89034d67c031e972efb17812934c1c
Leona <srt459vn31@gmail.com>
2016-08-12 06:57:41 +0000

    0812

--- PayMent/models/ConflictProcess.php
+++ PayMent/models/ConflictProcess.php
@@ -32,11 +32,13 @@ date_default_timezone_set("Asia/Taipei");
 
                 $this->pdo_connect->commit();
                 $this->pdo_connect = null;
+
                 return [true, $userId, $balance];
 
 
             } catch (Exception $err) {
                 $this->pdo_connect->rollBack();
+
                 return $err->getMessage();
                 $this->pdo_connect = null;
             }
